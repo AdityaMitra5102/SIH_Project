6de7092847e25870d67cd30729e386e919dc9d5f
Aditya Mitra <adityaarghya0@gmail.com>
2022-08-19 12:29:15 +0530

    Add files via upload

--- runner.sh
+++ runner.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
 sudo apt-get install python3 python3-dev python3-numpy python3-pip python3-opencv portaudio19-dev git ffmpeg 
 pip3 install --upgrade pip
-pip3 install jupyter pillow pydub soundfile numpy opencv-python uuid amodem ffmpeg-python cryptography
+pip3 install jupyter pillow pydub soundfile numpy opencv-python opencv-contrib-python uuid amodem ffmpeg-python cryptography
 jupyter notebook
