c67ff361e35afeafddfb933143d310b179f88e2c
riya <riyarai1511@gmail.com>
2022-08-26 10:32:54 +0530

    update

--- flasking/static/send.js
+++ flasking/static/send.js
@@ -1,36 +1,17 @@
 const uploadFile = document.getElementById("upload");
 const encrypt = document.getElementById('encrypt');
-const compress = document.getElementById('compress');
 const hidden = document.getElementById('hidden');
-const password = document.getElementById('password');
-const options = document.getElementById('options');
 const compressedArea = document.getElementsByClassName('compressed-area');
-const hideOnCompress = document.getElementById('on_compress');
 
-function upload_file(file) {
-    const file_length = (file.size) / 1000;
-    const duration = file_length / 1.96;
-    const time = duration/100;
-    let progress = document.querySelector('.progress');
-    let progressVal = 0;
-    const fakeUploadPerc = [0, 10, 25, 40, 45, 60, 70, 90, 100];
-
-    let interval = setInterval(() => {
-        progress.style.width = `${fakeUploadPerc[progressVal]}%`;
-        progressVal++;
-        if(progressVal === duration){
-            clearInterval(interval)
-        }
-    }, 1000)
-
-console.log(progress);
-
-    console.log(time);
-}
-
-const input = document.getElementById('myFile');
-
-const button = document.getElementById('input_btn');
+// function upload_file(file) {
+//     const file_length = (file.size) / 1000;
+//     const duration = file_length / 1.96;
+//     var i = 1;
+//     var progress = document.getElementsByClassName('progress');
+//     // progress.setAttribute('id', 'play-animation');
+//     progress.style.animation = `progress-animation ${duration/100}`;
+//     console.log(duration);
+// }
 
 button.onclick = (e) => {
     e.preventDefault();
@@ -42,28 +23,6 @@ button.onclick = (e) => {
     upload_file(file);
 }
 
-uploadFile.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const file = document.getElementById('myFile');
-const userFile = file.files[0];
-
-    const formData = new FormData();
-    formData.append('user-file', userFile);
-    formData.append('encryt-password', password.value);
-
-    // console.log(password.value);
-
-
-    fetch('https://httpbin.org/post', {
-        method: 'POST',
-        body: formData
-    })
-    .then(resp => resp.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
-
-});
 
 encrypt.addEventListener('change', (e) => {
     if (e.target.checked){
@@ -85,3 +44,61 @@ compress.addEventListener('change', (e) => {
         hideOnCompress.style.display = "none";
     }
 })
+
+function upload_file(file) {
+    const file_length = (file.size) / 1000;
+    const duration = file_length / 1.96;
+    const time = duration/100;
+    let progress = document.querySelector('.progress');
+    let progressVal = 0;
+    const fakeUploadPerc = [0, 10, 25, 40, 45, 60, 70, 90, 100];
+
+    let interval = setInterval(() => {
+        progress.style.width = `${fakeUploadPerc[progressVal]}%`;
+        progressVal++;
+        if(progressVal === duration){
+            clearInterval(interval)
+        }
+    }, 1000)
+
+console.log(progress);
+
+    console.log(time);
+}
+// const uploadFile = document.getElementById("upload");
+// const encrypt = document.getElementById('encrypt');
+// const compress = document.getElementById('compress');
+// const hidden = document.getElementById('hidden');
+// const password = document.getElementById('password');
+// const options = document.getElementById('options');
+// const compressedArea = document.getElementsByClassName('compressed-area');
+// const hideOnCompress = document.getElementById('on_compress');
+
+
+// const input = document.getElementById('myFile');
+
+// const button = document.getElementById('input_btn');
+
+
+// uploadFile.addEventListener("submit", (event) => {
+//     event.preventDefault();
+
+// //     const file = document.getElementById('myFile');
+// // const userFile = file.files[0];
+
+// //     const formData = new FormData();
+// //     formData.append('user-file', userFile);
+// //     formData.append('encryt-password', password.value);
+
+// //     // console.log(password.value);
+
+
+// //     fetch('https://httpbin.org/post', {
+// //         method: 'POST',
+// //         body: formData
+// //     })
+// //     .then(resp => resp.json())
+// //     .then(data => console.log(data))
+// //     .catch(err => console.log(err))
+
+// });
