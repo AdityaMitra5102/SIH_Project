af19f641c0700274e8eb1d35fb6685131fb338f8
riya <riyarai1511@gmail.com>
2022-08-26 10:05:32 +0530

    update frontend

--- flasking/static/send.js
+++ flasking/static/send.js
@@ -1,42 +1,69 @@
 const uploadFile = document.getElementById("upload");
 const encrypt = document.getElementById('encrypt');
+const compress = document.getElementById('compress');
 const hidden = document.getElementById('hidden');
+const password = document.getElementById('password');
+const options = document.getElementById('options');
 const compressedArea = document.getElementsByClassName('compressed-area');
+const hideOnCompress = document.getElementById('on_compress');
 
 function upload_file(file) {
     const file_length = (file.size) / 1000;
     const duration = file_length / 1.96;
-    const percent = duration /100;
-    // progressArea.stop().css("width", 0).animate({
-    //     width: 100
-    //   }, {
-    //     duration: duration,
-    //     progress: function(promise, progress, ms) {
-    //       $(this).text(Math.round(progress * 100) + '%');
-    //     }
-    //   });
-    var i = 1;
-    var progress = document.getElementsByClassName('progress');
-    // progress.setAttribute('id', 'play-animation');
-    progress.style.animation = `progress-animation ${percent}`;
-    // var interval = setInterval(function () {
-    //     i++;
-    //     progress.style.width = `${i}%`;
-    //     // i++;
-    //     // progressArea.css("width", i + "px");
-    //     if(i >= 100){
-    //         alert("done");
-    //         clearInterval(interval);
-    //     }
-    // }, 10)
-    console.log(duration);
+    const time = duration/100;
+    let progress = document.querySelector('.progress');
+    let progressVal = 0;
+    const fakeUploadPerc = [0, 10, 25, 40, 45, 60, 70, 90, 100];
+
+    let interval = setInterval(() => {
+        progress.style.width = `${fakeUploadPerc[progressVal]}%`;
+        progressVal++;
+        if(progressVal === duration){
+            clearInterval(interval)
+        }
+    }, 1000)
+
+console.log(progress);
+
+    console.log(time);
 }
 
-if (encrypt.checked){
-        hidden.style.display = "block";
-    } else {
-        hidden.style.display = "none";
+const input = document.getElementById('myFile');
+
+const button = document.getElementById('input_btn');
+
+button.onclick = (e) => {
+    e.preventDefault();
+
+    const file = document.getElementById('myFile');
+// const userFile = file.files[0];
+    input.click();
+    options.style.display = 'block';
+    upload_file(file);
 }
+
+uploadFile.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const file = document.getElementById('myFile');
+const userFile = file.files[0];
+
+    const formData = new FormData();
+    formData.append('user-file', userFile);
+    formData.append('encryt-password', password.value);
+
+    // console.log(password.value);
+
+
+    fetch('https://httpbin.org/post', {
+        method: 'POST',
+        body: formData
+    })
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+
+});
 
 encrypt.addEventListener('change', (e) => {
     if (e.target.checked){
@@ -44,4 +71,17 @@ encrypt.addEventListener('change', (e) => {
     } else {
         hidden.style.display = "none";
     }
-  });
+});
+
+compress.addEventListener('change', (e) => {
+
+    const file = document.getElementById('myFile');
+    const userFile = file.files[0];
+    
+    if (e.target.checked){
+        hideOnCompress.style.display = "block";
+        upload_file(userFile);
+    } else {
+        hideOnCompress.style.display = "none";
+    }
+})
